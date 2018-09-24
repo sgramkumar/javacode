6ec9251ae216509d69a6924c60c4370f1897aaee
Ramkumar SG <rsg@vmware.com>
2018-09-24 16:21:04 -0700

    mend

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 // This is Hello World Java Program
 public class HelloWorld {
     public static void main(String[] args) {
-        // Dummy hot fix here
         System.out.println("Hello World!");
     }
 }
