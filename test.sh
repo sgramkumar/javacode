f6dbc3b3dfaa35fc24eb74a1ab0e283891492b55
Ramkumar SG <rsg@vmware.com>
2018-12-20 15:20:28 -0800

    test

--- test.sh
+++ test.sh
@@ -7,5 +7,25 @@ then
     echo "inside if"
 fi
 
-echo "`date +%D`","1100" >> leaksummary.csv
+lines=`cat leaksummary.csv | wc -l`
+if [ "$lines" -gt "2" ]
+then
+    echo "fetching leak value from previous run"
+    previous_leak=`tail -1 leaksummary.csv | cut -d, -f2`
+    echo $previous_leak
+fi
+
+leak=3000
+echo "`date +%D`","$leak" >> leaksummary.csv
 cp leaksummary.csv /tmp
+
+if [ -f "./leak_alert" ]
+then
+    rm -f ./leak_alert
+fi
+leak_diff=`expr $leak - $previous_leak`
+if [ "$leak_diff" -gt "500" ]
+then
+    touch ./leak_alert
+fi
+
